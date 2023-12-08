7f845195cf145697b69051d8d87d4c81181fd348
user <user@localhost.localdomain>
2023-12-08 14:14:46 -0500

    improve formating of entrypoint and unlock tables at end

--- docker-entrypoint-initdb.d/80-set-master-from-slave.sh
+++ docker-entrypoint-initdb.d/80-set-master-from-slave.sh
@@ -4,10 +4,11 @@ set -e
 
 if [ $MARIADB_ROLE = "slave" ]; then
 
-echo "Setting server ID on slave"
+echo "Setting server ID on slave to 2"
 mariadb -uroot -p$MARIADB_ROOT_PASSWORD \
     --execute="SET GLOBAL server_id = 2;"
 sed -i 's/server_id=1/server_id=2/g' /etc/mysql/mariadb.conf.d/80-master-or-slave.cnf
+
 
 # Configuring the master node. We run this from the slave to allow a chronological 
 # order of opperations so the slave cannot proceed until master is configured.
@@ -20,9 +21,6 @@ mariadb -uroot -p$MARIADB_ROOT_PASSWORD -h mariadb-master \
 
 mariadb-dump -uroot -p$MARIADB_ROOT_PASSWORD -h mariadb-master --all-databases --master-data > /tmp/mariadb-backup/master_initialization.sql
 
-mariadb -uroot -p$MARIADB_ROOT_PASSWORD -h mariadb-master \
-    --execute="UNLOCK TABLES;"
-
 
 # Run the following on the slave node (localhost)
 
@@ -31,9 +29,21 @@ mariadb -uroot -p$MARIADB_ROOT_PASSWORD < /tmp/mariadb-backup/master_initializat
 
 echo "Setting replication host on slave"
 mariadb -uroot -p$MARIADB_ROOT_PASSWORD \
-    --execute="CHANGE MASTER TO MASTER_HOST='mariadb-master', MASTER_USER='replication_user', MASTER_PASSWORD='DVvh5lnR1iqok1CV0cAd', MASTER_PORT=3306, MASTER_LOG_FILE='master1-bin.000002', MASTER_LOG_POS=689, MASTER_USE_GTID = slave_pos, MASTER_CONNECT_RETRY=10;"
+    --execute="CHANGE MASTER TO \
+                    MASTER_HOST='mariadb-master',\
+                    MASTER_USER='replication_user',\
+                    MASTER_PASSWORD='DVvh5lnR1iqok1CV0cAd',\
+                    MASTER_PORT=3306,\
+                    MASTER_LOG_FILE='master1-bin.000002',\
+                    MASTER_LOG_POS=689,\
+                    MASTER_USE_GTID = slave_pos,\
+                    MASTER_CONNECT_RETRY=10;"
 
 echo "Start slave"
 mariadb -uroot -p$MARIADB_ROOT_PASSWORD --execute="START SLAVE; SHOW SLAVE STATUS \G"
+
+# Unlock tables on master only after slave is configured
+mariadb -uroot -p$MARIADB_ROOT_PASSWORD -h mariadb-master \
+    --execute="UNLOCK TABLES;"
 
 fi
