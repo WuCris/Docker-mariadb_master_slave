86e5588338b1ecc57705894cf29c39a25467c33a
user <user@localhost.localdomain>
2023-12-10 21:04:18 -0500

    make MYSQL specific changes

--- docker-entrypoint-initdb.d/80-set-master-from-slave.sh
+++ docker-entrypoint-initdb.d/80-set-master-from-slave.sh
@@ -5,7 +5,7 @@ set -e
 if [ $MYSQL_ROLE = "master" ]; then
     echo "Creating replication user and backup of Master"
     mysql -uroot -p$MYSQL_ROOT_PASSWORD \
-        --execute="CREATE USER 'replication_user'@'%' IDENTIFIED BY '$MYSQL_REPLICATION_PASS'; \
+        --execute="CREATE USER 'replication_user'@'%' IDENTIFIED WITH mysql_native_password BY '$MYSQL_REPLICATION_PASS'; \
         GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%'; FLUSH PRIVILEGES; FLUSH TABLES WITH READ LOCK;" 
 fi
 
@@ -14,9 +14,10 @@ if [ $MYSQL_ROLE = "slave" ]; then
     # These settings won't apply until entrypoint exits and temporary server is stopped.
     echo -e "\nread-only=1" >> /etc/mysql/conf.d/80-master-or-slave.cnf
 
+    echo "Waiting for master database..."
     sleep 10s # Give master time to initialize
     echo "Backing up master host"
-    mysql-dump -uroot -p$MYSQL_ROOT_PASSWORD -h mysql-master --all-databases --master-data | zstd > /tmp/mysql-backup/master_initialization.sql.zstd
+    mysqldump -uroot -p$MYSQL_ROOT_PASSWORD -h mysql-master --all-databases --master-data | zstd > /tmp/mysql-backup/master_initialization.sql.zstd
 
 
     echo "Importing master database to slave"
@@ -36,11 +37,7 @@ if [ $MYSQL_ROLE = "slave" ]; then
                         MASTER_CONNECT_RETRY=10;"
 
     echo "Start slave"
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD --execute="START SLAVE;"
-
-    # GTID must be enabled after intial connection to prevent user creation error
-    echo "Enabling GTID and restarting slave"
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD --execute="STOP SLAVE; CHANGE MASTER TO master_use_gtid = slave_pos; START SLAVE; SHOW SLAVE STATUS \G"
+    mysql -uroot -p$MYSQL_ROOT_PASSWORD --execute="START SLAVE; SHOW SLAVE STATUS \G"
 
     # Unlock tables on master only after slave is configured
     mysql -uroot -p$MYSQL_ROOT_PASSWORD -h mysql-master \
